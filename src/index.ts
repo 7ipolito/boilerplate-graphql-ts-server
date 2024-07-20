e619f07e52ce3746024b8b314483c8dad1c246c3
Allan Hipólito <allanhipolito66@gmail.com>
2024-07-19 23:09:20 -0300

    feat: to hash password, to change to apollo client

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,28 @@
-
-
-import { createServer } from 'node:http'
-import { createSchema, createYoga } from 'graphql-yoga'
-import { loadSchemaSync } from '@graphql-tools/load'
-import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
-
-import { join } from 'node:path'
-import { AppDataSource } from './data-source'
+import { ApolloServer } from 'apollo-server';
+import { loadSchemaSync } from '@graphql-tools/load';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { join } from 'node:path';
+import { AppDataSource } from './data-source';
+import { resolvers } from './resolvers';
 
 async function startServer() {
+  // Carrega o schema GraphQL do arquivo
+  const typeDefs = loadSchemaSync(join(__dirname, 'schema.graphql'), {
+    loaders: [new GraphQLFileLoader()],
+  });
 
-const typeDefs = loadSchemaSync(join(__dirname, 'schema.graphql'), {
-  loaders: [new GraphQLFileLoader()],
-});
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-};
-const yoga = createYoga({
-  schema: createSchema({typeDefs:typeDefs,resolvers})
-})
+  // Cria a instância do Apollo Server
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
+  await AppDataSource.initialize();
 
-await AppDataSource.initialize();
+  // Inicia o servidor Apollo
+  const { url } = await server.listen({ port: 4000 });
 
-const server = createServer(yoga)
-
-server.listen(4000, () => {
-  console.info('Server is running on http://localhost:4000/graphql')
-})
-
+  console.info(`Server is running on ${url}`);
 }
 
 startServer();
-
-
-
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,7 +1,31 @@
-type Query {
-  hello: String
+import { hash } from "bcryptjs";
+import { User } from "./entity/User";
+import { IResolvers } from "@graphql-tools/utils";
+
+interface RegisterArgs {
+  email: string;
+  password: string;
 }
 
-schema {
-  query: Query
-}
+export const resolvers: IResolvers = {
+  Query:{
+    users: async (): Promise<User[]> => {
+      const user = await User.find()
+      return user;
+    },
+  },
+  Mutation: {
+    register: async (_: any, args: RegisterArgs): Promise<boolean> => {
+      const { email, password } = args;
+      const hashedPassword = await hash(password, 10);
+      const user = User.create({
+        email,
+        password: hashedPassword,
+      });
+
+      await user.save()
+
+      return true;
+    },
+  },
+};
