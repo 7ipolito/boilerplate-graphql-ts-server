b5621cbff02831f36cbd70f6b55e13b28bb11c1b
Allan Hipólito <allanhipolito66@gmail.com>
2024-07-28 15:56:54 -0300

    feat: to add validation erros with yup, to fix some dependencies

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,7 +1,14 @@
 import { hash } from "bcryptjs";
 import { IResolvers } from "@graphql-tools/utils";
 import { User } from "../../entity/User";
+import * as yup from "yup"
+import { formatYupError } from "../../utils/formatYupError";
+import { duplicateEmail, emailNotLongEnough, invalidEmail, passwordNotLongEnough } from "./errorMessages";
 
+const schema = yup.object().shape({
+  email: yup.string().min(3, emailNotLongEnough).max(255).email(invalidEmail),
+  password: yup.string().min(3, passwordNotLongEnough).max(255)
+});
 interface RegisterArgs {
   email: string;
   password: string;
@@ -10,13 +17,18 @@ interface RegisterArgs {
 export const resolvers: IResolvers = {
   Mutation: {
     register: async (_: any, args: RegisterArgs): Promise<any> => {
+      try {
+        await schema.validate(args, {abortEarly:false})
+      } catch (err:any) {
+       return formatYupError(err)
+      }
       const { email, password } = args;
       const userAlreadyExists = await User.findOne({where:{email}, select:["id"]})
       if(userAlreadyExists){
         return [
           {
             path: "email",
-            message:"already taken"
+            message:duplicateEmail
           }
         ]
       }
